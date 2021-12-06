d83575be2043b20c43f0def31448d795572c7759
XueWenjing <15551036718@qq.com>
2021-12-06 15:55:52 +0800

    Employee

--- src/main/java/chapter05/Employee.java
+++ src/main/java/chapter05/Employee.java
@@ -2,4 +2,14 @@ package chapter05;
 
 public class Employee {
 
+    private String name;
+    private double salary;
+
+    public Employee(){
+
+    }
+    public Employee(String n, double s){
+        name = n;
+        salary = s;
+    }
 }
