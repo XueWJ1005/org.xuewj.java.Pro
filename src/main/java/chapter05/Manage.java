d4a2413507965b212f4d0ed1bb4fc1d24bb25a29
XueWenjing <15551036718@qq.com>
2021-12-07 17:47:04 +0800

    Manage

--- src/main/java/chapter05/Manage.java
+++ src/main/java/chapter05/Manage.java
@@ -2,11 +2,20 @@ package chapter05;
 
 public class Manage extends Employee{
 
-    private String name;
-    private double salary;
+
+    private double bonus;
 
     public Manage(String name, double salary){
         super(name, salary);
+        bonus = 0;
+    }
+
+    public double getSalary(){
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+    public void setBonus(double b){
+        bonus = b;
     }
 
 
