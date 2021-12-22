02fa680db980dbff5002853f95303782637cbd0c
XueWenjing <15551036718@qq.com>
2021-12-22 11:01:05 +0800

    enum

---
+++ src/main/java/chapter05/Test.java
@@ -0,0 +1,8 @@
+package chapter05;
+
+public class Test {
+    public static void main(String[] args) {
+        double largest = Double.NEGATIVE_INFINITY;
+        System.out.println(largest);
+    }
+}
