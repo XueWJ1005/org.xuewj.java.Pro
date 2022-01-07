aab07a7f58992e31678e659beed380824871d335
XueWenjing <15551036718@qq.com>
2022-01-07 17:58:40 +0800

    lambda

--- src/main/java/chapter06/lambda/LambdaTest.java
+++ src/main/java/chapter06/lambda/LambdaTest.java
@@ -1,7 +1,25 @@
 package chapter06.lambda;
 
+import javax.swing.*;
+import java.sql.Time;
+import java.util.Arrays;
+import java.util.Date;
+
 public class LambdaTest {
     public static void main(String[] args) {
+        String[] planets = new String[]{"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted in dictionary order: ");
+        Arrays.sort(planets);
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted by length: ");
+        Arrays.sort(planets, (first, second) -> first.length() - second.length());
+        System.out.println(Arrays.toString(planets));
 
+        Timer t = new Timer(10000, event -> System.out.println("The time is " + new Date()));
+        t.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program");
+        System.exit(0);
     }
 }
